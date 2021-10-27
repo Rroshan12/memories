a01dedf49ecfe0ca4408bfd08f5d3f85cc085028
Rroshan12 <grappleroshan7@gmail.com>
2021-10-28 03:44:06 +0545

    Login system added with user control

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
 
@@ -13,11 +14,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
-app.get('/',(req,res) =>{
-
-  res.send('Hello to memories Api')
-})
+app.use("/user", userRouter);
 
 const CONNECTION_URL = 'mongodb+srv://Jshero:Jshero123@cluster0.x4qnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
