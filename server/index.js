7cc4a6b3476d42fb4776b9b2e59735d635839a78
Rroshan12 <grappleroshan7@gmail.com>
2021-10-28 07:02:38 +0545

    updated

--- server/index.js
+++ server/index.js
@@ -13,6 +13,10 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.get('/', (req,res) =>{
+  res.send("Heello memories Api");
+});
+
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
